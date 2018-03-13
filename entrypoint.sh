e226992970cb80fb50df851bd76b0cde11762435
Josh Pollara <joshpollara@gmail.com>
2018-03-13 21:01:14 +0100

    attendees arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-/eventbrite-cli.py
+/eventbrite-cli.py "$@"
--- eventbrite-cli.py
+++ eventbrite-cli.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 import os
+import sys
 from eventbrite import Eventbrite
 
-eventbrite = Eventbrite(os.environ['OAUTH_TOKEN'])
-attendees = eventbrite.get_event_attendees(os.environ['EVENT_ID'])
-print(attendees)
+def main(arg):
+    if arg == 'attendees':
+        eventbrite = Eventbrite(os.environ['OAUTH_TOKEN'])
+        attendees = eventbrite.get_event_attendees(os.environ['EVENT_ID'])
+        return attendees
+
+if __name__ == '__main__':
+    print(main(sys.argv[1]))
